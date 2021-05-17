c3869076da60d4a7000fc759b8f4841a88525183
Firliandy <lian.eddy@gmail.com>
2021-05-17 19:34:02 +0700

    plnTopup init

---
+++ src/navigation/stackNavigator/plnStackNavigator.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const plnStackNavigator = () => {};
+
+export default plnStackNavigator;
